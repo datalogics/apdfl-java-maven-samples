f8cd0081f9487ba91968d2feaac0d783227bb9e4
datalogics-saharay <saharay@datalogics.com>
2024-12-12 10:42:53 -0600

    Do not build/run ConvertToOffice sample on Mac

--- tasks.py
+++ tasks.py
@@ -175,6 +175,9 @@ def run_samples(ctx):
 
         if 'DocToImages' in sample or 'ImageDisplayByteArray' in sample:
             continue
+        if platform.system() == 'Darwin' and ('ConvertToOffice' in sample):
+            print(f'{sample} not available on this OS')
+            continue
         if platform.system() == 'Darwin' and ('ConvertToOffice' in sample or
                                               'CreateDocFromXPS' in sample or
                                               'ConvertXFAToAcroForms' in sample
